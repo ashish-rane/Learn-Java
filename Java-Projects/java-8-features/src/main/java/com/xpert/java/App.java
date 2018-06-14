d1410f7a440ea568701c6c087e672900e0fe3c7c
Ashish <ashishrane@outlook.com>
2018-06-14 20:02:42 -0300

    Java-Basics Java-Datastructures commit

---
+++ Java-Projects/java-8-features/src/main/java/com/xpert/java/App.java
@@ -0,0 +1,13 @@
+package com.xpert.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        // TODO: Lambda expressions, ? operator, Method References,
+    }
+}
---
+++ Java-Projects/java-net-demo/src/main/java/com/xpert/java/App.java
@@ -0,0 +1,15 @@
+package com.xpert.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+
+        // TODO: Sockets, HTTP GET and POST.
+    }
+}
